09f964eee35f1e5038c0275a40f6be0216a6ade3
priyaA01 <pryshanthi.it@gmail.com>
2018-02-04 11:14:15 -0600

    updated final version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TrainScheduler
-Train Scheduler
+
+ Train scheduler application incorporates Firebase to host arrival and departure data.
+
+ This app will retrieve and manipulate information with Moment.js and provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
+
+
+ ### Priya Ammaiyappan
+
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,7 @@ var config = {
 };
 firebase.initializeApp(config);
 
+//reference to firebase database
 var database = firebase.database();
 
 var trainName = "";
@@ -27,7 +28,8 @@ $("#submitBtn").on("click", function (event) {
 	firstTrainTime = $("#firstTrainTime").val();
 	frequency = $("#frequency").val().trim();
 
-	database.ref().push({
+	//data pushed to firebase database
+	database.ref("/trains").push({
 		trainName: trainName,
 		destination: destination,
 		firstTrainTime: firstTrainTime,
@@ -36,33 +38,38 @@ $("#submitBtn").on("click", function (event) {
 	});
 });
 
-database.ref().on("child_added", function (childsnapshot) {
+/*everytime when a new dataset is added to firebase database, 
+a new row is dynamically created displaying the dataset along 
+with the minutes away and next arrival time calculated using momentJS*/
+database.ref("/trains").on("child_added", function (childsnapshot) {
 
-	$("#trainName").val("");
-	$("#destination").val("");
-	$("#firstTrainTime").val("");
-	$("#frequency").val("");
+		$("#trainName").val("");
+		$("#destination").val("");
+		$("#firstTrainTime").val("");
+		$("#frequency").val("");
 
-	var trainNameFB = childsnapshot.val().trainName;
-	var destinationFB = childsnapshot.val().destination;
-	var frequencyFB = childsnapshot.val().frequency;
-	var firstTrainTimeFB = childsnapshot.val().firstTrainTime;
+		var trainNameFB = childsnapshot.val().trainName;
+		var destinationFB = childsnapshot.val().destination;
+		var frequencyFB = childsnapshot.val().frequency;
+		var firstTrainTimeFB = childsnapshot.val().firstTrainTime;
 
-	var firstTimeConverted = moment(firstTrainTimeFB, "hh:mm").subtract(1, "years");
-	var currentTime = moment();
-	var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-	var tRemainder = diffTime % frequencyFB;
-	var minutesAwayFB = frequencyFB - tRemainder;
-	var nextArrivalFB = moment().add(minutesAwayFB, "minutes");
+		var firstTimeConverted = moment(firstTrainTimeFB, "hh:mm").subtract(1, "years");
+		var currentTime = moment();
+		var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+		var tRemainder = diffTime % frequencyFB;
+		var minutesAwayFB = frequencyFB - tRemainder;
+		var nextArrivalFB = moment().add(minutesAwayFB, "minutes");
 
-	var newRow = $("<tr>")
-	newRow.append("<td>" + trainNameFB + "</td>");
-	newRow.append("<td>" + destinationFB + "</td>");
-	newRow.append("<td>" + frequencyFB + "</td>");
-	newRow.append("<td>" + moment(nextArrivalFB).format("hh:mm") + "</td>");
-	newRow.append("<td>" + minutesAwayFB + "</td>");
-	newRow.append("</tr>");
-	$("tbody").append(newRow);
+		var newRow = $("<tr>")
+		newRow.append("<td>" + trainNameFB + "</td>");
+		newRow.append("<td>" + destinationFB + "</td>");
+		newRow.append("<td>" + frequencyFB + "</td>");
+		newRow.append("<td>" + moment(nextArrivalFB).format("hh:mm") + "</td>");
+		newRow.append("<td>" + minutesAwayFB + "</td>");
+		newRow.append("</tr>");
+		$("tbody").append(newRow);
+	}, function (errorObject) {
+		console.log('Errors handled: ' + errorObject.code);
+	}
 
-});
-
+);
